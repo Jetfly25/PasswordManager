0ea4b76478b068757cecd50660f6b7bc75c5d668
Miguel Fuentes <mafuentes250@gmail.com>
2025-07-13 19:33:45 -0600

    Added user account to track list of websites with saved passwords

---
+++ passwordmanager/src/main/java/com/passwordmanager/passwordmanagerlauncher/user/UserInternetAccount.java
@@ -0,0 +1,54 @@
+package com.passwordmanager.passwordmanagerlauncher.user;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UserInternetAccount {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+    private String URL;
+    private String username;
+    private String password;
+
+    public UserInternetAccount() {}
+
+    public UserInternetAccount(String URL, String username, String password){
+        this.URL = URL;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getID(){
+        return ID;
+    }
+
+    public void setID(Long ID){
+        this.ID = ID;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getURL(){
+        return URL;
+    }
+
+    public void setURL(String URL){
+        this.URL = URL;
+    }
+}
