6e329c74a1b52fae738b0ff9487972e471e171f0
Miguel Fuentes <mafuentes250@gmail.com>
2025-07-20 14:33:47 -0600

    Added new requirement for password creation

--- passwordmanager/src/main/resources/static/passwordValidator.js
+++ passwordmanager/src/main/resources/static/passwordValidator.js
@@ -1,11 +1,13 @@
 function checkPasswordStrength() {
     const password = document.getElementById("password").value;
     const strengthIndicator = document.getElementById("passwordStrength");
+    const containsWhitespace = (str) => /\s/.test(str);
 
     let isValidPasswordLength = false;
     let isValidLetter = false;
     let isValidNumber = false;
     let isValidSpecialChar = false;
+    let hasWhiteSpace = false;
 
     if (password.length >= 8) {
         isValidPasswordLength = true;
@@ -23,16 +25,22 @@ function checkPasswordStrength() {
         isValidSpecialChar = true
     }
 
+    if (containsWhitespace(password)){
+        hasWhiteSpace = true;
+    }
+
+    strengthIndicator.style.backgroundColor = "#ff4d4d";
     if (!isValidPasswordLength) {
         strengthIndicator.textContent = "Password must be at least 8 characters!";
-        strengthIndicator.style.backgroundColor = "#ff4d4d";
     } else if (!isValidLetter) {
         strengthIndicator.textContent = "Password must contain at least one letter! (A-Z)";
     } else if (!isValidNumber) {
         strengthIndicator.textContent = "Password must contain at least one letter! (0-9)";
     } else if (!isValidSpecialChar) {
         strengthIndicator.textContent = "Password must contain at least one special character! (!, ?, @, etc.)";
-    } else {
+    } else if (hasWhiteSpace){
+        strengthIndicator.textContent = "Password must not have spaces!";
+    } else if (isValidPasswordLength && isValidLetter && isValidSpecialChar && isValidNumber && !hasWhiteSpace){
         strengthIndicator.textContent = "Good!";
         strengthIndicator.style.backgroundColor = "#4caf50";
     }
